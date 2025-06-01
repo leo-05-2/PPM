92aeb2947a69cf040dabc5c696d1a0a8f72a22ec
leo <leonardo.viti2@edu.unifi.it>
2025-06-01 17:34:09 +0200

    FEAT: added cart view to change the address

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Cart
+from .forms import CartForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+@login_required
+def update_cart_address(request):
+    cart ,created = Cart.objects.get_or_create(user=request.user)
+
+    if request.method == 'POST':
+        form = CartForm(request.POST, instance=cart, user=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('cart:cart_detail')  #todo: implement
+    else:
+        form = CartForm(instance=cart, user=request.user)
+
+    return render(request, 'cart/update_cart_address.html', {'form': form, 'cart': cart})
