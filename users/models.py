d58d5f76a002fb686c33049d1aaf010d0b8311d1
leo <leonardo.viti2@edu.unifi.it>
2025-06-01 17:27:37 +0200

    FIX: fixed user model

--- users/models.py
+++ users/models.py
@@ -1,16 +1,32 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 # Create your models here.
 
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, password=None, **extra_fields):
+        if not username:
+            raise ValueError('The Username field must be set')
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(username, password, **extra_fields)
+
 class CustomUser(AbstractUser):
-    delivery_address = models.ForeignKey(
-        'AddressList',
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True,
-        related_name='delivery_addresses'
-    )
+   phone_number = models.CharField(max_length=15, blank=True, null=True)
+   payment_method = models.CharField(max_length=50, blank=True, null=True)
+   objects = CustomUserManager()
 
     def __str__(self):
         return self.username
