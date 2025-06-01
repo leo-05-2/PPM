73dcdc63cca9f9f9bafe47ef116af0d70dbc6b1d
leo <leonardo.viti2@edu.unifi.it>
2025-06-01 17:33:45 +0200

    FEAT: added cart form to change the address

--- cart/forms.py
+++ cart/forms.py
@@ -1,3 +1,4 @@
+from crispy_forms.helper import FormHelper
 from django import forms
 from .models import Cart, AddressList
 
@@ -8,6 +9,15 @@ class CartForm(forms.ModelForm):
         fields = ['address']  # Assuming you want to allow users to select an address for their cart
 
     def __init__(self, *args, **kwargs):
-        user = kwargs.pop('user')
+        user = kwargs.pop('user',None)
         super().__init__(*args, **kwargs)
-        self.fields['address'].queryset = AddressList.objects.filter(user=user)
+        if user: self.fields['address'].queryset = AddressList.objects.filter(user=user)
+        elif self.instance and self.instance.pk and self.instance.user:
+            self.fields['address'].queryset = AddressList.objects.filter(user=self.instance.user)
+        else:
+            self.fields['address'].queryset = AddressList.objects.none()
+
+
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.add_input( forms.Submit('submit', 'Update Cart Address'))
