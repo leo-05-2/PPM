471f45dc499196c76fb37deb1debe3f7f582150b
leo <leonardo.viti2@edu.unifi.it>
2025-06-23 17:45:13 +0200

    FIX: fixed error from a call to usable password

--- users/admin.py
+++ users/admin.py
@@ -6,21 +6,13 @@ from users.forms import CustomUserCreationForm, CustomUserChangeForm
 # Register your models here.
 
 
+
 class CustomUserAdmin(UserAdmin):
-
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
-    model = CustomUser
-
-    list_display = UserAdmin.list_display + ('phone_number', 'payment_method')
-
-
-    fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('phone_number', 'payment_method',)}),
-    )
-    # Se vuoi aggiungere il campo anche quando crei un nuovo utente dal pannello admin
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        (None, {'fields': ('phone_number', 'payment_method',)}),
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'phone_number', 'payment_method'),
+        }),
     )
 class AddressAdmin(admin.ModelAdmin):
     list_display = ('nickname', 'street', 'city', 'postal_code', 'country')
