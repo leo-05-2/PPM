e6d0590f5d90cb17c5f9f783b1fc9fafc5b5860e
leo <leonardo.viti2@edu.unifi.it>
2025-06-25 09:36:37 +0200

    INFO: fixies

--- PPM/urls.py
+++ PPM/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path( '', include('core.urls', namespace='core') ),
@@ -25,4 +26,4 @@ urlpatterns = [
     path('cart/', include('cart.urls', namespace='cart') ),
 
     path('products/', include('products.urls', namespace='products') ),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
