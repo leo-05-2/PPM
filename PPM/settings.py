5dfba7b423de496b8dfdd8ac17c9e8d8ec9f5eb4
leo <leonardo.viti2@edu.unifi.it>
2025-07-04 20:35:57 +0200

    INFO: fixes

--- PPM/settings.py
+++ PPM/settings.py
@@ -67,7 +67,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.runserver_nostatic'
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
